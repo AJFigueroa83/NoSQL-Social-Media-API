a45ca98566c80b8fce72129afe6e0421334a6673
Alfredo Figueroa <alfredojfigueroa@gmail.com>
2022-07-04 00:24:50 -0400

    created the user model.

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+const usersSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false
+    }
+)
+
+usersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const Users = model('Users', usersSchema);
+
+module.exports = Users;
